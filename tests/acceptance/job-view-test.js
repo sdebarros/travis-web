75179d4555024611dfd2e02113e593fb440abc0f
lislis <mail@lislis.de>
2016-07-12 17:21:15 +0200

    implement View Config
    first step: write a test that displays the config
    of the first job as a string
    
    add some styles
    
    add child routes and templates
    
    add config tab to actual job view
    
    start using same tab markup for all tabs
    
    make job tabs a component
    
    add ember-prism to config json
    
    link to job routes all the time
    
    use same tab styles everywhere
    
    change `trigger build` to `restart build` in build actions
    
    hack for job config log view
    
    fix ESLint errors
    
    add different tabs styles
    
    add active class to first tab is it is not set by the current route

--- tests/acceptance/job-view-test.js
+++ tests/acceptance/job-view-test.js
@@ -2,16 +2,24 @@ import { test } from 'qunit';
 import moduleForAcceptance from 'travis/tests/helpers/module-for-acceptance';
 import jobPage from 'travis/tests/pages/job';
 import buildPage from 'travis/tests/pages/build';
+import jobTabs from 'travis/tests/pages/job-tabs';
 
-moduleForAcceptance('Acceptance | job view');
+
+moduleForAcceptance('Acceptance | job view', {
+  beforeEach() {
+    const currentUser = server.create('user');
+    signInUser(currentUser);
+  }
+});
 
 test('visiting job-view', function (assert) {
   let repo =  server.create('repository', { slug: 'travis-ci/travis-web' });
   // create branch
   server.create('branch', {});
+
   let commit = server.create('commit', { author_email: 'mrt@travis-ci.org', author_name: 'Mr T', committer_email: 'mrt@travis-ci.org', committer_name: 'Mr T', branch: 'acceptance-tests', message: 'This is a message', branch_is_default: true });
   let build = server.create('build', { repository_id: repo.id, state: 'passed', commit_id: commit.id, commit });
-  let job = server.create('job', { number: '1234.1', repository_id: repo.id, state: 'passed', build_id: build.id, commit, build });
+  let job = server.create('job', { number: '1234.1', repository_id: repo.id, state: 'passed', build_id: build.id, commit, build, config: { language: 'Hello' } });
   commit.job = job;
 
   job.save();
@@ -29,6 +37,17 @@ test('visiting job-view', function (assert) {
     assert.equal(jobPage.author, 'Mr T authored and committed');
 
     assert.equal(jobPage.log, 'Hello log');
+
+    assert.ok(jobTabs.logTab.isShowing, 'displays the job log');
+    assert.ok(jobTabs.configTab.isHidden, 'config is hidden');
+  });
+
+  jobTabs.configTab.click();
+
+  andThen(function () {
+    assert.ok(jobTabs.configTab.isShowing, 'displays the config');
+    assert.ok(jobTabs.logTab.isHidden, 'log is hidden');
+    assert.equal(jobTabs.configTab.contents, '[ { \"language\": \"Hello\" } ]');
   });
 });
 
